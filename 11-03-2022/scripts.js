fa266af81f58cf2f76e3b1c30643bd441c43e991
Mo'ath Bdour <moathbdour@users.noreply.github.com>
2022-11-03 15:04:42 +0200

    index file

---
+++ 11-03-2022/scripts.js
@@ -0,0 +1,84 @@
+
+let mark1 = -50
+
+var you;
+if (mark1 < 0) 
+{
+  you = "invalid input your mark is less than 0 you are too bad";
+} 
+else if (mark1 >= 0 && mark1 < 50)
+ {
+  you = "faild";
+ }
+ else if (mark1 >= 50 && mark1 < 60)
+ {
+  you = "d";
+ }
+ else if (mark1 >= 60 && mark1 < 70)
+ {
+  you = "c";
+ }
+ else if (mark1 >= 70 && mark1 < 80)
+ {
+  you = "b";
+ }
+ else if (mark1 >= 80 && mark1 < 90)
+ {
+  you = "a";
+ }
+ else if (mark1 >= 90 && mark1 <= 100)
+ {
+  you = "A+";
+ }
+ else if (mark1 >= 100 )
+ {
+  you = "invalid input";
+ }
+
+
+
+
+
+
+ let mark2 = 95
+
+let you2;
+if (mark2 < 0) 
+{
+  you2 = "invalid input your mark is less than 0 you are too bad";
+} 
+else if (mark2 >= 0 && mark2 < 50)
+ {
+  you2 = "faild";
+ }
+ else if (mark2 >= 50 && mark2 < 60)
+ {
+  you2 = "d";
+ }
+ else if (mark2 >= 60 && mark2 < 70)
+ {
+  you2 = "c";
+ }
+ else if (mark2 >= 70 && mark2 < 80)
+ {
+  you2 = "b";
+ }
+ else if (mark2 >= 80 && mark2 < 90)
+ {
+  you2 = "a";
+ }
+ else if (mark2 >= 90 && mark2 <= 100)
+ {
+  you2 = "A+";
+ }
+ else if (mark2 >= 100 )
+ {
+  you2 = "invalid input";
+ }
+
+
+
+
+
+
+
