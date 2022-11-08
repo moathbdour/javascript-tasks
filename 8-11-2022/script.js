0b3a6d2ba353236f320803b1b7de64540dd6a1a6
Mo'ath Bdour <moathbdour@users.noreply.github.com>
2022-11-08 20:42:37 +0200

    index file

---
+++ 8-11-2022/script.js
@@ -0,0 +1,22 @@
+document.getElementById("firstP").innerHTML="Group4";
+document.getElementsByTagName("p")[1].innerHTML="is the best";
+document.getElementsByClassName("sec")[0].innerHTML="in the word";
+// document.getElementById("img1").setAttribute("width","200px");
+document.getElementById("img1").width="100";
+
+function block3(){
+    var jj=document.getElementById("img2")
+    jj.style.display="block"
+
+}
+function block4(){
+    var jj=document.getElementById("img2")
+    jj.style.display="none"
+
+}
+  document.querySelector("p.example").style.backgroundColor = "red";
+//  const nodeList = document.querySelectorAll(".example");
+//  for (let i = 0; i < nodeList.length; i++) {
+//    nodeList[i].style.backgroundColor = "red";
+// }
+
